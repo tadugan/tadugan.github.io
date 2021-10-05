0b4fbc136da6a604a2c6d7adcfa48b179d88abf9
Tim Dugan <tadugan@me.com>
2021-10-05 14:07:27 -0500

    Added About page image

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,21 @@
-
+import { Grid } from '@mui/material';
+import picture from '/Users/Tim/Desktop/Prime Academy/Tier IV/react-portfolio/src/TimDuganPic.jpeg';
+import './About.css';
 
 function About() {
   return (
-    <div>
-        <p>About Page</p>
+    <div className="about-body">
+        <Grid
+            container 
+            spacing={1} 
+            direction="column" 
+            justifyContent="center" 
+            alignItems="center"
+        >
+            <Grid item xs={6}>
+                <img className="about-picture" src={picture} alt="Tim Dugan" />
+            </Grid>    
+        </Grid>
     </div>
   );
 }
