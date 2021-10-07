37611138befe30e6020dd4d787bdae732073b90d
Tim Dugan <tadugan@me.com>
2021-10-07 15:24:05 -0500

    added min height to Portfolio and Contact pages

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import './Contact.css';
 
 function Contact() {
     return (
-      <div>
+      <div className="contact-body">
           <p>Contact Page</p>
       </div>
     );
