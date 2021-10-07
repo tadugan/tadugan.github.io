eef382aaec42ee6a059019ca3d635c7eba845856
Tim Dugan <tadugan@me.com>
2021-10-07 14:11:00 -0500

    Added ThemeProvider to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+import { createTheme, ThemeProvider } from "@mui/material";
 import React from "react";
 import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import About from '../About/About';
@@ -6,19 +7,31 @@ import Contact from '../Contact/Contact';
 import Portfolio from '../Portfolio/Portfolio';
 import './App.css';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#455a64',
+      light: '#718792',
+      dark: '#1c313a',
+    },
+  },
+});
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <AppBarNav />
-        <Switch>
-          <Redirect exact from="/" to="/about" />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Route exact path="/contact" component={Contact} />
-        </Switch>
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div className="App">
+          <AppBarNav />
+          <Switch>
+            <Redirect exact from="/" to="/about" />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/contact" component={Contact} />
+          </Switch>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
