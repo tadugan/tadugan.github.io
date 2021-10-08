623fb8c02b7368ff7788fc402698f8fd6b1f93ec
Tim Dugan <tadugan@me.com>
2021-10-08 15:11:12 -0500

    Added portfolio header

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,11 +1,22 @@
+import { Grid, Typography } from "@mui/material";
 import React from "react";
 import './Portfolio.css';
 
 function Portfolio() {
     return (
-      <div className="portfolio-body">
-          <p>Portfolio Page</p>
-      </div>
+        <div className="portfolio-body">
+          <Grid
+            container
+            spacing={0}
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <Typography variant="h3" component="h2">Project Gallery</Typography>
+            </Grid>
+          </Grid>
+        </div>
     );
   }
   
