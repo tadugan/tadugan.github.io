5290be136bd0653c6249196daab67b52cf21453f
Tim Dugan <tadugan@me.com>
2021-10-08 16:17:03 -0500

    Map through projectArray, passed props into ProjectCard component, added prop details to ProjectCard

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -3,12 +3,23 @@ import React from "react";
 import ProjectCard from "../ProjectCard/ProjectCard";
 import './Portfolio.css';
 
+const projectArray = [
+  {
+    name: 'Boardgame Achievement Tracker',
+    description: 'Videogame Achievements are a popular component of Videogaming. Adding extra objectives and challenges encourage players to try new play styles and increase the replayability of videogames. This application takes that same idea and applies it to Board Games and Tabletop Games.',
+    image_url: 'https://image.shutterstock.com/image-photo/single-ear-corn-isolated-on-260nw-793795156.jpg',
+    video_url: 'https://www.youtube.com/',
+    code_url: 'https://github.com/tadugan',
+    deployment_url: 'https://id.heroku.com/login'
+  }
+]
+
 function Portfolio() {
     return (
         <div className="portfolio-body">
           <Grid
             container
-            spacing={0}
+            spacing={2}
             direction="column"
             justifyContent="center"
             alignItems="center"
@@ -19,14 +30,18 @@ function Portfolio() {
             <Grid
               item
               container
-              spacing={0}
+              spacing={3}
               direction="row"
               justifyContent="center"
               alignItems="center"
             >
-              <Grid item>
-                  <ProjectCard />
-              </Grid>
+              {projectArray.map((project, index) => {
+                return (
+                  <Grid item key={index}>
+                    <ProjectCard project={project}/>
+                  </Grid>
+                );
+              })}
             </Grid>
           </Grid>
         </div>
--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,9 +1,59 @@
 import React from "react";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import './ProjectCard.css';
+import { Grid } from "@mui/material";
 
-function ProjectCard() {
+function ProjectCard( {project} ) {
     return (
-        <p>Project Card</p>
+        <Card sx={{ maxWidth: 400 }}>
+            <CardMedia
+                component="img"
+                height="160"
+                image="https://image.shutterstock.com/image-photo/single-ear-corn-isolated-on-260nw-793795156.jpg"
+                alt="Project image"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                {project.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                {project.description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Grid
+                    container
+                    direction="row"
+                    spacing={2}
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    {project.video_url &&
+                        <Grid item>
+                            <a href={project.video_url} target="_blank" rel="noopener noreferrer" className="project-card-button">
+                                <Button size="small">Video Demo</Button>
+                            </a>
+                        </Grid>}
+                    {project.code_url &&
+                        <Grid item>
+                            <a href={project.code_url} target="_blank" rel="noopener noreferrer" className="project-card-button">
+                                <Button size="small">View Code</Button>
+                            </a>
+                        </Grid>}
+                    {project.deployment_url &&
+                        <Grid item>
+                            <a href={project.deployment_url} target="_blank" rel="noopener noreferrer" className="project-card-button">
+                                <Button size="small">View Deployed App</Button>
+                            </a>
+                        </Grid>}
+                </Grid>
+            </CardActions>
+        </Card>
     );
 }
 
